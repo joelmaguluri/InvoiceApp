c715403ca9844415b3af47aa32c508fc360ce6c4
sudeep987 <msudeep.joel@gmail.com>
2019-03-03 07:34:05 +0530

    invoice

---
+++ src/Components/Center.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+
+class Center extends Component {
+  render() {
+    return (
+   <h1>Hello</h1>
+ );
+  }
+}
+
+export default Center;
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Center from './Components/Center';
+import App from './Components/App';
+ReactDOM.render(<App />, document.getElementById('details'));
+ReactDOM.render(<Center />, document.getElementById('center'));
